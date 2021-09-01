2f3d19227ecb97aeccc62197d339b44584a200c7
BarrelBrenner <rondolph@aol.com>
2021-08-31 22:13:54 -0500

    adjusted client side of life

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mernshopping",
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb+srv://Glacier-Freeze:Uribe5050@basic-barrel.puqzm.mongodb.net/mernshopping?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
