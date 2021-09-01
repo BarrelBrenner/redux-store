d9dd41091373470a2edb0b2945d93f5159fcfc5c
BarrelBrenner <rondolph@aol.com>
2021-08-31 20:13:09 -0500

    adjusted package json dependencies

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/react-hooks';
+import { useQuery } from '@apollo/client';
 import Cart from '../components/Cart';
 import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY, ADD_TO_CART, UPDATE_PRODUCTS, } from '../utils/actions';
 import { QUERY_PRODUCTS } from '../utils/queries';
